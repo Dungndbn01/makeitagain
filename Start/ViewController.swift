3907341a7ee9112598bab7d201a11ad7ec37e700
dinhdung.bacninh@gmail.com <nguyendinhdung@Nguyens-MacBook-Pro.local>
2017-10-10 13:13:32 +0700

    Commit All

--- Start/ViewController.swift
+++ Start/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblHienThi: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblHienThi.textColor = UIColor.black
+        lblHienThi.numberOfLines = 3
         // Do any additional setup after loading the view, typically from a nib.
     }
 
